26b69cc750deb6227a9c97fe433db26cb95ad107
Nguyễn Khắc Vũ <chodiem.io@gmail.com>
2022-10-16 14:54:44 +0700

    update

--- src/chat/gateway/chat.gateway.ts
+++ src/chat/gateway/chat.gateway.ts
@@ -1,29 +1,38 @@
 import {
+  MessageBody,
   OnGatewayConnection,
   OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { OnModuleInit, UseGuards } from '@nestjs/common';
-import { Server } from 'socket.io';
+import { Logger, OnModuleInit, UseGuards } from '@nestjs/common';
+import { Server, Socket } from 'socket.io';
+import { JwtGuard } from 'src/auth/guard';
+import { AuthDto } from 'src/auth/dto';
+import { GetUser } from 'src/auth/decorator';
 
-@WebSocketGateway({ cors: {} })
-export class ChatGateway
-//   implements OnGatewayConnection, OnGatewayDisconnect, OnModuleInit
-{
-  constructor(
-  ) // private conversationService: ConversationService,
-  {}
-  //   onModuleInit() {
-  //     this.conversationService
-  //       .removeActiveConversations()
-  //       .pipe(take(1))
-  //       .subscribe();
-  //     this.conversationService.removeMessages().pipe(take(1)).subscribe();
-  //     this.conversationService.removeConversations().pipe(take(1)).subscribe();
-  //   }
-
+// @UseGuards(JwtGuard)
+@WebSocketGateway(3006, { cors: true })
+// @WebSocketGateway()
+export class ChatGateway implements OnModuleInit {
   @WebSocketServer()
   server: Server;
+
+  onModuleInit() {
+    this.server.on('connection', (socket) => {
+      console.log(socket.id);
+      console.log('Connected');
+    });
+  }
+
+  @SubscribeMessage('newMessage')
+  onNewMessage(@MessageBody() body: any) {
+    console.log(body);
+    this.server.emit('onMessage', {
+      msg: "New message",
+      content: body
+    })
+  }
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
-import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 import { PrismaService } from '../src/prisma/prisma.service';
 import * as pactum from 'pactum';
